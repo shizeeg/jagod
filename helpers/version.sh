5187ad621b7413cfc0348ca22040d974ccc5d582
sh!zeeg <shizeeque@gmail.com>
2014-11-21 15:43:43 +0300

    fix spaces in helpers/version.sh

--- helpers/version.sh
+++ helpers/version.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
-COMMIT=$(git log -1 --abbrev-commit | grep 'commit' | sed 's|commit\s*||g')
-COMMITDATE=$(git log -1 --date=short | grep '[Dd]ate'| sed 's|[Dd]ate:\s*||g')
-CNT=$(git log | grep 'commit' | wc -l)
-VERSION=$(echo "0.0.${CNT} (GIT" "${COMMIT} (${COMMITDATE}))")
+COMMIT=$(git log -1 --abbrev-commit | grep 'commit' | cut -d ' ' -f2)
+COMMITDATE=$(git log -1 --date=short | grep '[Dd]ate'| sed 's|[^0-9-]||g')
+CNT=$(git log | grep 'commit' | wc -l | tr -d ' ')
+VERSION=$(echo "0.0.${CNT} (GIT ${COMMIT} (${COMMITDATE}))")
 sed "s|BOTVERSION = ".*"|BOTVERSION = \"$VERSION\"|g" uname.go > tmp.go
 mv tmp.go uname.go
+echo "$COMMIT"
+echo "$COMMITDATE"
+echo "$CNT"
+echo "$VERSION"
