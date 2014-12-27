c4e12e07593b79d32e783f3afca81e5afa69d475
sh!zeeg <shizeeque@gmail.com>
2014-12-28 01:55:02 +0300

    [plugin]: yandex ("tr" and "spell" commands reimplemented as a plugin)

--- plugins/yandex.go
+++ plugins/yandex.go
@@ -2,51 +2,71 @@ package main
 
 import (
 	"encoding/xml"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+
+	"github.com/shizeeg/ini"
 )
 
-type TrTag struct {
-	Pos  string   `xml:"pos,attrib,omitempty"`
-	Text string   `xml:"text"`
-	Syn  []string `xml:"syn>text,omitempty"`
-	Mean []string `xml:"mean>text,omitempty"`
-	Ex   []string `xml:"ex>text,omitempty"`
-	Extr []string `xml:"ex>tr>text,omitempty"`
+var (
+	lang   string
+	apikey string
+	banner = "http://api.yandex.ru/dictionary/"
+)
+
+func init() {
+	cfg, err := ini.Load("/etc/yandex.cfg")
+	if err != nil {
+		log.Println(err)
+	}
+
+	flag.StringVar(&lang, "lang", cfg.Section("").Key("LANG").In("ru", []string{}), "Localization")
+	flag.StringVar(&apikey, "apikey", cfg.Section("").Key("APIKEY").String(), "Yandex Dict.API Key")
+	flag.Parse()
 }
 
-type DefTag struct {
-	Pos  string  `xml:"pos,attr,omitempty"`
-	Ts   string  `xml:"ts,attr,omitempty"`
-	Text string  `xml:"text"`
-	Tr   []TrTag `xml:"tr"`
+func main() {
+	fmt.Println(banner)
+	if flag.NArg() < 1 || len(flag.Arg(0)) < 5 {
+		usage(lang)
+		return
+	}
+
+	switch {
+	case flag.Arg(0) == "list":
+		fmt.Println(strings.Join(GetLangs(apikey), ", "))
+		return
+	case flag.Arg(0) == "spell":
+		fmt.Println(YandexSpell("", strings.Join(flag.Args()[1:], " ")))
+		return
+	case flag.Arg(0) == "help":
+		usage(lang)
+		return
+	}
+
+	fmt.Println(YandexDic(apikey, lang, flag.Arg(0), flag.Arg(1)))
 }
 
-type DicResult struct {
-	Def []DefTag `xml:"def"`
-}
-
-type LangResult struct {
-	String []string `xml:"string"`
-}
-
-type SpellResult struct {
-	// XMLName xml.Name `xml:"SpellResult"`
-	Errors []SpellError `xml:"error,omitempty"`
-}
-
-type SpellError struct {
-	Code  string   `xml:"code,attr"`
-	Pos   string   `xml:"pos,attr"`
-	Row   string   `xml:"col,attr"`
-	Len   string   `xml:"len,attr"`
-	Words []string `xml:"word"`
-	S     []string `xml:"s,omitempty"`
+func usage(lang string) {
+	if lang != "ru" {
+		fmt.Fprintln(os.Stderr, "  tr <from-to> <word>")
+		fmt.Fprintln(os.Stderr, "  spell <text>")
+	} else {
+		fmt.Fprintln(os.Stderr, "  tr <из-в> <слово>")
+		fmt.Fprintln(os.Stderr, "  spell <текст>")
+	}
 }
 
 // GetLangs returns possible language pairs for YandexDic()
 func GetLangs(apikey string) []string {
+	if apikey == "" {
+		log.Fatalln("Can't get \"APIKEY\" environment variable!")
+	}
 	langsurl := fmt.Sprintf("https://dictionary.yandex.net/api/v1/dicservice/getLangs?key=%s", apikey)
 	resp, err := http.Get(langsurl)
 	if err != nil {
@@ -54,7 +74,7 @@ func GetLangs(apikey string) []string {
 	}
 	defer resp.Body.Close()
 	dec := xml.NewDecoder(resp.Body)
-	var r LangResult
+	var r langresult
 	if err := dec.Decode(&r); err != nil {
 		return []string{err.Error()}
 	}
@@ -73,7 +93,7 @@ func YandexSpell(langs, text string) string {
 	}
 
 	dec := xml.NewDecoder(resp.Body)
-	var r SpellResult
+	var r spellresult
 	if err := dec.Decode(&r); err != nil {
 		return err.Error()
 	}
@@ -97,6 +117,10 @@ func YandexSpell(langs, text string) string {
 // YandexDic translates message to given lang-lang pair.
 // call GetLangs() to get possible lang pairs
 func YandexDic(apikey, resplang, lang, text string) string {
+	if apikey == "" {
+		log.Fatalln("No Yandex dict \"APIKEY\" defined!")
+	}
+
 	text = url.QueryEscape(text)
 	if len(resplang) < 2 {
 		resplang = "en"
@@ -109,7 +133,7 @@ func YandexDic(apikey, resplang, lang, text string) string {
 
 	defer resp.Body.Close()
 	dec := xml.NewDecoder(resp.Body)
-	var d DicResult
+	var d dicresult
 	if err := dec.Decode(&d); err != nil {
 		return err.Error()
 	}
@@ -117,8 +141,8 @@ func YandexDic(apikey, resplang, lang, text string) string {
 }
 
 // String is a pretty printer for DicResult
-func (r *DicResult) String() string {
-	tr := func(d *DefTag) string {
+func (r *dicresult) String() string {
+	tr := func(d *deftag) string {
 		var s string
 		for _, tr := range d.Tr {
 			s = s + fmt.Sprintf("%s, ", tr.Text)
@@ -136,4 +160,42 @@ func (r *DicResult) String() string {
 		}
 	}
 	return str
+}
+
+type trtag struct {
+	Pos  string   `xml:"pos,attrib,omitempty"`
+	Text string   `xml:"text"`
+	Syn  []string `xml:"syn>text,omitempty"`
+	Mean []string `xml:"mean>text,omitempty"`
+	Ex   []string `xml:"ex>text,omitempty"`
+	Extr []string `xml:"ex>tr>text,omitempty"`
+}
+
+type deftag struct {
+	Pos  string  `xml:"pos,attr,omitempty"`
+	Ts   string  `xml:"ts,attr,omitempty"`
+	Text string  `xml:"text"`
+	Tr   []trtag `xml:"tr"`
+}
+
+type dicresult struct {
+	Def []deftag `xml:"def"`
+}
+
+type langresult struct {
+	String []string `xml:"string"`
+}
+
+type spellresult struct {
+	// XMLName xml.Name `xml:"SpellResult"`
+	Errors []spellerror `xml:"error,omitempty"`
+}
+
+type spellerror struct {
+	Code  string   `xml:"code,attr"`
+	Pos   string   `xml:"pos,attr"`
+	Row   string   `xml:"col,attr"`
+	Len   string   `xml:"len,attr"`
+	Words []string `xml:"word"`
+	S     []string `xml:"s,omitempty"`
 }
