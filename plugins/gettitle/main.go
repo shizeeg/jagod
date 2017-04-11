271724aaeb8299a8ff8edecfb9fab26195d66c9e
sh!zeeg <shizeeque@gmail.com>
2017-04-11 22:47:48 +0300

    [plugin] `gettitle` show youtube video link duration when possible

--- plugins/gettitle/main.go
+++ plugins/gettitle/main.go
@@ -11,22 +11,31 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/shizeeg/ini"
+	"github.com/shizeeg/youtube"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/charset"
 )
 
 var (
+	duration    string
 	encoding    string
 	contType    string
 	lang        string
 	link        string
 	exclude     string
+	youtubeKey  string
 	showHeaders bool
 	prefix      = "Заголовок"
 	unkPrefix   = "HTTP заголовок"
 )
 
 func init() {
+	cfg, err := ini.Load("/etc/jagod.cfg")
+	if err != nil {
+		log.Fatalf("can't find config file %v\n", err)
+	}
+	flag.StringVar(&youtubeKey, "key", cfg.Section("google").Key("youtubeDataAPIKey").String(), "YouTube Data API key")
 	flag.StringVar(&encoding, "oe", "utf8", "Output Encoding")
 	flag.StringVar(&lang, "lang", "ru", "Language to output in")
 	flag.BoolVar(&showHeaders, "headers", false, "Show HTTP Headers")
@@ -54,6 +63,10 @@ func main() {
 	}
 
 	res, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
 
 	contType = res.Header.Get("Content-Type")
 	if showHeaders || len(contType) >= 9 && contType[0:9] != "text/html" {
@@ -77,10 +90,17 @@ func main() {
 		title, _ = getTag(text, "title")
 	}
 	title = strings.Trim(title, "\n ")
-	if len(title) > 0 {
-		fmt.Print(prefix + ": " + title)
+	if title == "" {
+		return
 	}
-	res.Body.Close()
+	if youtubeKey != "" && strings.HasSuffix(title, "- YouTube") {
+		if ids := youtube.IDs(link); len(ids) > 0 {
+			if dur, err := youtube.GetDuration(youtubeKey, ids[0]); err == nil && dur != "" {
+				title = fmt.Sprintf("%s [%s]", title, dur)
+			}
+		}
+	}
+	fmt.Println(prefix + ": " + title)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -94,7 +114,7 @@ func getTag(r io.Reader, tag string) (data string, err error) {
 		// token type
 		tokenType := d.Next()
 		if tokenType == html.ErrorToken {
-			return "", errors.New("Token Error!")
+			return "", errors.New("token error")
 		}
 		token := d.Token()
 		switch tokenType {
